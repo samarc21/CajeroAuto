7072abbce04b8c2ae6a6a0103b8d3aaa50639c6b
Sara Centeno <marcelacenteno@gmail.com>
2020-08-02 23:30:15 +0200

    Se agregaron botones con numeros

--- CajeroAuto/Form1.Designer.cs
+++ CajeroAuto/Form1.Designer.cs
@@ -28,13 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.btn_0 = new MetroFramework.Controls.MetroButton();
+            this.btn_9 = new MetroFramework.Controls.MetroButton();
+            this.btn_8 = new MetroFramework.Controls.MetroButton();
+            this.btn_7 = new MetroFramework.Controls.MetroButton();
+            this.btn_6 = new MetroFramework.Controls.MetroButton();
+            this.btn_5 = new MetroFramework.Controls.MetroButton();
+            this.btn_4 = new MetroFramework.Controls.MetroButton();
+            this.btn_3 = new MetroFramework.Controls.MetroButton();
+            this.btn_2 = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // btn_1
+            // 
+            this.btn_1.Location = new System.Drawing.Point(313, 227);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(42, 47);
+            this.btn_1.TabIndex = 9;
+            this.btn_1.Text = "1";
+            this.btn_1.UseSelectable = true;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(649, 167);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(42, 47);
+            this.metroButton1.TabIndex = 10;
+            this.metroButton1.Text = "metroButton1";
+            this.metroButton1.UseSelectable = true;
+            // 
+            // btn_0
+            // 
+            this.btn_0.Location = new System.Drawing.Point(361, 367);
+            this.btn_0.Name = "btn_0";
+            this.btn_0.Size = new System.Drawing.Size(42, 47);
+            this.btn_0.TabIndex = 11;
+            this.btn_0.Text = "0";
+            this.btn_0.UseSelectable = true;
+            // 
+            // btn_9
+            // 
+            this.btn_9.Location = new System.Drawing.Point(409, 314);
+            this.btn_9.Name = "btn_9";
+            this.btn_9.Size = new System.Drawing.Size(42, 47);
+            this.btn_9.TabIndex = 12;
+            this.btn_9.Text = "9";
+            this.btn_9.UseSelectable = true;
+            // 
+            // btn_8
+            // 
+            this.btn_8.Location = new System.Drawing.Point(361, 314);
+            this.btn_8.Name = "btn_8";
+            this.btn_8.Size = new System.Drawing.Size(42, 47);
+            this.btn_8.TabIndex = 13;
+            this.btn_8.Text = "8";
+            this.btn_8.UseSelectable = true;
+            // 
+            // btn_7
+            // 
+            this.btn_7.Location = new System.Drawing.Point(313, 314);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(42, 47);
+            this.btn_7.TabIndex = 14;
+            this.btn_7.Text = "7";
+            this.btn_7.UseSelectable = true;
+            // 
+            // btn_6
+            // 
+            this.btn_6.Location = new System.Drawing.Point(409, 271);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(42, 47);
+            this.btn_6.TabIndex = 15;
+            this.btn_6.Text = "6";
+            this.btn_6.UseSelectable = true;
+            // 
+            // btn_5
+            // 
+            this.btn_5.Location = new System.Drawing.Point(361, 271);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(42, 47);
+            this.btn_5.TabIndex = 16;
+            this.btn_5.Text = "5";
+            this.btn_5.UseSelectable = true;
+            // 
+            // btn_4
+            // 
+            this.btn_4.Location = new System.Drawing.Point(313, 271);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(42, 47);
+            this.btn_4.TabIndex = 17;
+            this.btn_4.Text = "4";
+            this.btn_4.UseSelectable = true;
+            // 
+            // btn_3
+            // 
+            this.btn_3.Location = new System.Drawing.Point(409, 227);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(42, 47);
+            this.btn_3.TabIndex = 18;
+            this.btn_3.Text = "3";
+            this.btn_3.UseSelectable = true;
+            // 
+            // btn_2
+            // 
+            this.btn_2.Location = new System.Drawing.Point(361, 227);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(42, 47);
+            this.btn_2.TabIndex = 19;
+            this.btn_2.Text = "2";
+            this.btn_2.UseSelectable = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_2);
+            this.Controls.Add(this.btn_3);
+            this.Controls.Add(this.btn_4);
+            this.Controls.Add(this.btn_5);
+            this.Controls.Add(this.btn_6);
+            this.Controls.Add(this.btn_7);
+            this.Controls.Add(this.btn_8);
+            this.Controls.Add(this.btn_9);
+            this.Controls.Add(this.btn_0);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.btn_1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroButton btn_1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton btn_0;
+        private MetroFramework.Controls.MetroButton btn_9;
+        private MetroFramework.Controls.MetroButton btn_8;
+        private MetroFramework.Controls.MetroButton btn_7;
+        private MetroFramework.Controls.MetroButton btn_6;
+        private MetroFramework.Controls.MetroButton btn_5;
+        private MetroFramework.Controls.MetroButton btn_4;
+        private MetroFramework.Controls.MetroButton btn_3;
+        private MetroFramework.Controls.MetroButton btn_2;
     }
 }
 
